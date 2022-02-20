1aab8d1e648162cd3e2924c68ae7b173b5d0c190
okamoto-contenna <96048603+Kousuke-Okamoto@users.noreply.github.com>
2022-02-20 12:41:36 +0900

    ライブリロード実装１

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 // gulpfile.js
-const { src, dest } = require('gulp');
+const { src, dest, watch, series, parallel }  = require('gulp');
+//src:入力先
+//dest:出力先
+//watch：変更を監視する
+//series：処理を直列で処理（順番に処理したいときに使う）
+//parallel：処理を並列で処理（順番は問わない）
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
@@ -8,6 +13,7 @@ const autoprefixer = require('autoprefixer');
 const cssdeclsort = require('css-declaration-sorter');
 const gcmq = require('gulp-group-css-media-queries');
 const mode = require('gulp-mode')();//開発モードとプロダクションモードの使い分けをできるようにする
+const browserSync = require('browser-sync');//ライブリロード用
 
 const compileSass = (done) => {
     // postcssを纏めて変数に代入
@@ -30,6 +36,33 @@ const compileSass = (done) => {
     done();
    };
 
+
+   const buildServer = (done) => {
+    browserSync.init({
+      port: 3000,//localhost:8080を開く
+      files: ["**/*"],//全てのファイルを監視
+      // 静的サイト
+      server: { baseDir: './' },//index.htmlがどこにあるか
+      // 動的サイト
+      // proxy: "http://localsite.local/",
+      open: true,　//ブラウザを自動で開く
+      watchOptions: {
+        debounceDelay: 1000,//1秒間タスクの連続実行を抑制
+      },
+    });
+    done();
+   };
+   
+   const browserReload = done => {
+    browserSync.reload();
+    done();
+   };
+   
+   const watchFiles = () => {
+    watch( './src/scss/**/*.scss', series(compileSass, browserReload))
+   };
+
 module.exports = {
  sass: compileSass,
+ default: parallel(buildServer, watchFiles),
 };
