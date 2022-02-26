dc69d763788074bcb6f0504f09b91a44a74fbcb1
okamoto-contenna <96048603+Kousuke-Okamoto@users.noreply.github.com>
2022-02-26 20:04:46 +0900

    サンプル作成１

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,9 @@
 // main.js
 import $ from 'jquery';
 import { showAlert } from "./sub";
+import { header} from './components/_header';
 
 showAlert();
+header();
+
 $("#js-alert-button").css("font-size", "30px");
